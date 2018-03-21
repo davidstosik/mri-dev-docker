98935f67f983373c63f584a4d444d507524b7f7c
David Stosik <david.stosik+git-noreply@gmail.com>
2018-03-21 16:24:52 +0000

    Redirect errors properly in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 IMAGE_NAME=mri_dev
 VOLUME_NAME=mri_dev_vol
 
-docker volume inspect $VOLUME_NAME > /dev/null 2>1
+docker volume inspect $VOLUME_NAME > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Docker volume '$VOLUME_NAME' already exists, you may want to delete it first."
   exit 1
 fi
 
-docker image inspect $IMAGE_NAME > /dev/null 2>1
+docker image inspect $IMAGE_NAME > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Docker image '$IMAGE_NAME' already exists, you may want to delete it first."
   exit 1
