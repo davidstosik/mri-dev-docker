418b255e35be0e9e80a7bd1181a383df599416dd
David Stosik <david.stosik+git-noreply@gmail.com>
2018-03-21 14:15:02 +0000

    Get gdb to work in Docker
    https://stackoverflow.com/questions/35860527/warning-error-disabling-address-space-randomization-operation-not-permitted?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa#comment62818827_35860527
    
    Closes #2

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 docker run \
+  --cap-add=SYS_PTRACE \
+  --security-opt seccomp=unconfined \
   -it --rm \
   --mount src=mri_dev_vol,target=/workdir \
   --mount src=$PWD/ruby,target=/ruby,type=bind \
