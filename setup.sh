1b1f1cbab90307dad5f42f2f39878f46c2a4893e
David Stosik <david.stosik+git-noreply@gmail.com>
2018-03-23 11:53:37 +0000

    Give the option to delete Docker volume/image in setup.sh
    Clses #4

--- setup.sh
+++ setup.sh
@@ -3,16 +3,36 @@
 IMAGE_NAME=mri_dev
 VOLUME_NAME=mri_dev_vol
 
+yn_question() {
+  read -p "$1 [yN] " REPLY
+  case "$REPLY" in
+    y*|Y*) true ;;
+    *) false ;;
+  esac
+}
+
 docker volume inspect $VOLUME_NAME > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-  echo "Docker volume '$VOLUME_NAME' already exists, you may want to delete it first."
-  exit 1
+  echo "Docker volume '$VOLUME_NAME' already exists!"
+  if yn_question "Do you want to delete it?"; then
+    echo "Deleting Docker volume '$VOLUME_NAME'..."
+    docker volume rm $VOLUME_NAME
+  else
+    echo "Aborted."
+    exit 1
+  fi
 fi
 
 docker image inspect $IMAGE_NAME > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-  echo "Docker image '$IMAGE_NAME' already exists, you may want to delete it first."
-  exit 1
+  echo "Docker image '$IMAGE_NAME' already exists!"
+  if yn_question "Do you want to delete it?"; then
+    echo "Deleting Docker image '$IMAGE_NAME'..."
+    docker rmi $IMAGE_NAME
+  else
+    echo "Aborted."
+    exit 1
+  fi
 fi
 
 docker volume create $VOLUME_NAME
